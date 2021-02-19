866d91c4325190a900dd28bce6657372aff1c173
Timo Zhang <ztnuaa0211@gmail.com>
2021-02-20 00:11:07 +0800

    Add pack script for BluePrints Mod

---
+++ packBlueprints.ps1
@@ -0,0 +1,2 @@
+& 'D:\Program Files (x86)\Steam\steamapps\common\Dead Cells\ModTools\CDBTool.exe' -Collapse -Indir .\ExpandedCDB-BP\ -OutCDB ".\Expanded-BP\data.cdb"
+& 'D:\Program Files (x86)\Steam\steamapps\common\Dead Cells\ModTools\PAKTool.exe' -creatediffpak -refpak "D:\Program Files (x86)\Steam\steamapps\common\Dead Cells\res.pak" -indir "D:\Timo\Games\Timos-armory-mod\Expanded-BP\" -OutPak "D:\Timo\Games\Timos-armory-mod\workshop\Blueprints\res.pak"
