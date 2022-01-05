5ac7dd1118eefbc3c97feeb3da5173008df31cab
Timo Zhang <ztnuaa0211@gmail.com>
2022-01-05 10:36:05 +0800

    OneCellUnclock: Fix bugs in the script. Test is passed in the game.

--- oneCellUnlock.py
+++ oneCellUnlock.py
@@ -71,7 +71,7 @@ def main():
                 if "cellCost" in line:
                     cost = int(line.split(":")[-1].split(",")[0])
                     if cost > 1:
-                        lines[n] = "  cellCost: 1,\n"
+                        lines[n] = '  "cellCost": 1,\n'
                         # Write to file
                         desFile = file.replace("CDB-BP","CDB-OCU")
                         with open(desFile,'w') as f2:
@@ -98,24 +98,6 @@ def main():
         item = item[6:item.index(".")]
         mutations.append(item)
     
-    
-
-    
-    # Read base room file
-    f = open('src/PrisonFlaskRoom-BP.json','r')
-    lines = f.readlines()
-    f.close()
-
-    # Create new room file
-    f = open("PrisonFlaskRoom-BP-Mod.json",'w')
-    for n, line in enumerate(lines):
-        if "]," in line:
-            lines[n-1] = "    },\n"
-            header = lines[0:n]
-            tail = lines[n:]
-            break
-    f.writelines(header)
-
     # Generate Armory list
     # Remove duplicates
     weapons = []
@@ -140,72 +122,7 @@ def main():
     print("      Skills : {0}".format(sizeOfSkills))
     print("      Skins  : {0}".format(sizeOfSkins))
     print("      Metas  : {0}".format(sizeOfMetas))
-    '''
-    # Armory showcase at y direction
-    yList = [32,35,38,41,44,47]
-
-    # Weapons showcase
-    xRange = [2,34]
-    nInLine = int((xRange[-1]-xRange[0])/2)
-    for j in range(len(yList)):
-        for i in range(nInLine):
-            weaponIndex = j*nInLine+i
-            if weaponIndex < sizeOfWeapons:
-                x = xRange[0]+i*2+1
-                y = yList[j]
-                f.writelines(jsonWrapper(x,y,weapons[weaponIndex]))
-
-    # Skills showcase 1
-    yList = [35,38,41,44,47]
-    xRange = [36,62]
-    nInLine = int((xRange[-1]-xRange[0])/2)
-    for j in range(len(yList)):
-        for i in range(nInLine):
-            skillIndex = j*nInLine+i
-            if skillIndex < sizeOfSkills:
-                x = xRange[0]+i*2+1
-                y = yList[j]
-                f.writelines(jsonWrapper(x,y,skills[skillIndex]))
-
-    # Skins showcase
-    yList = [3,6,9]
-    xRange = [2,61]
-    nInLine = int((xRange[-1]-xRange[0])/2)
-    for j in range(len(yList)):
-        for i in range(nInLine):
-            skinIndex = j*nInLine+i
-            if skinIndex < len(skins):
-                x = xRange[0]+i*2+1
-                y = yList[j]
-                f.writelines(jsonWrapper(x,y,skins[skinIndex]))
-
-    # Metas and Mutation showcase
-    comb = metas+mutations
-    yList = [50,53,56]
-    xRange = [2,62]
-    nInLine = int((xRange[-1]-xRange[0])/2)
-    for j in range(len(yList)):
-        for i in range(nInLine):
-            combIndex = j*nInLine+i
-            if combIndex < len(comb):
-                x = xRange[0]+i*2+1
-                y = yList[j]
-                if comb[combIndex] in metas:
-                    # Runes
-                    f.writelines(jsonWrapperLoot(x,y,comb[combIndex]))
-                else:
-                    # Mutations
-                    f.writelines(jsonWrapper(x,y,comb[combIndex]))
-   
-    f.writelines(tail)
-
-    f.close()
-
-    # Copy file to ExtendedCDB
-    os.system("cp PrisonFlaskRoom-BP-Mod.json ./ExpandedCDB-BP/room/Prison/0573---PrisonFlaskRoom.json")
-
     print("Done!")
-    '''
 
 if __name__  == "__main__":
     main()
--- pack1CellUnlock.ps1
+++ pack1CellUnlock.ps1
@@ -1,2 +1,2 @@
-& 'D:\Program Files (x86)\Steam\steamapps\common\Dead Cells\ModTools\CDBTool.exe' -Collapse -Indir .\ExpandedCDB-OCU\ -OutCDB ".\Expanded-BP\data.cdb"
-& 'D:\Program Files (x86)\Steam\steamapps\common\Dead Cells\ModTools\PAKTool.exe' -creatediffpak -refpak "D:\Program Files (x86)\Steam\steamapps\common\Dead Cells\res.pak" -indir "D:\Timo\Games\Timos-armory-mod\Expanded-BP\" -OutPak "D:\Timo\Games\Timos-armory-mod\workshop\1CellUnlock\res.pak"
+& 'D:\Program Files (x86)\Steam\steamapps\common\Dead Cells\ModTools\CDBTool.exe' -Collapse -Indir .\ExpandedCDB-OCU\ -OutCDB ".\onecell-res\data.cdb"
+& 'D:\Program Files (x86)\Steam\steamapps\common\Dead Cells\ModTools\PAKTool.exe' -creatediffpak -refpak "D:\Program Files (x86)\Steam\steamapps\common\Dead Cells\res.pak" -indir "D:\Timo\Games\Timos-armory-mod\onecell-res\" -OutPak "D:\Timo\Games\Timos-armory-mod\workshop\OneCellUnlock\res.pak"
